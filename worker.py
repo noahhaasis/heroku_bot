0dcbe8cbe8b4ff5f1d9a53202ef70f0bca5698db
noahhaasis <haasis_noah@yahoo.de>
2018-02-11 05:19:57 +0100

    Remove line.

--- worker.py
+++ worker.py
@@ -116,7 +116,6 @@ async def on_message(message):
     elif not message.author.bot and '!plan' in message.content:
         # Create and send the table as an image
         plan_text = get_table()
-        plan_text = os.linesep.join([s for s in plan_text.splitlines() if s])
         img = Image.new('RGB', (500, 10 + 20*len(plan_text.splitlines())), color = (255, 255, 255))
         d = ImageDraw.Draw(img)
         d.text((10,10), plan_text, fill=(0,0,0))
